f0c6a5ff37698e2c773f6be59902e744c65503ff
Meheretabe Abayneh <meraman675@gmail.com>
2026-01-13 00:29:47 +0300

    Prepare project for Render deployment

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import django
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+# Run migrations automatically
+try:
+    call_command('migrate', interactive=False)
+    call_command('collectstatic', interactive=False, clear=True)
+except Exception as e:
+    print("Auto-deploy migration/static error:", e)
+
 
 application = get_wsgi_application()
